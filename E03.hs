5688afeb6fd72e1324e0de09add56abe1e4de984
Marco Pampaloni <crybot@hotmail.it>
2016-02-16 00:07:29 +0100

    Added problem 03

---
+++ E03.hs
@@ -0,0 +1,7 @@
+-- Find the K'th element of a list. The first element in the list is number
+-- 1.
+
+elementAt :: [a] -> Int -> a
+elementAt (x:_) 1 = x
+elementAt (x:xs) n | n > 1 = elementAt xs (n-1)
+elementAt _ _ = error "error"
